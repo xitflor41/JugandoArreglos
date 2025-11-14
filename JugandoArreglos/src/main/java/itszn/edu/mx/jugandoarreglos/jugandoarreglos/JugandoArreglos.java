b0b1e53b23121cba52940029754907c182760795
Xitlalic Guadalupe Flores Salcedo <128668402+xitflor41@users.noreply.github.com>
2025-11-14 13:31:09 -0600

    Add comment for webhook testing

--- JugandoArreglos/src/main/java/itszn/edu/mx/jugandoarreglos/jugandoarreglos/JugandoArreglos.java
+++ JugandoArreglos/src/main/java/itszn/edu/mx/jugandoarreglos/jugandoarreglos/JugandoArreglos.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
+/*Probando  webhook :)*/
 package itszn.edu.mx.jugandoarreglos.jugandoarreglos;
 import java.util.Arrays;
 
